426b5657d12723a8dc037541c89d7f7793f92212
surendrademo <72486790+surendrademo@users.noreply.github.com>
2024-04-24 13:56:53 +0530

    Related commit(s): Add / root for ui

--- index.js
+++ index.js
@@ -3,14 +3,13 @@
 const dbConnection = require("./src/utils/dbConnection.js");
 const { MONGODB_URI } = require("./config.js");
 const cors = require("cors");
-const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const express = require("express");
+const path = require("path");
 
 // Local files import
 const route = require("./src/routes/index.js");
 const imageRoute = require("./src/routes/imageRoutes.js");
-const user = require("./src/models/userModel.js");
 
 // Used for notification so don't remove
 const admin = require("./src/firebase/index.js");
@@ -19,6 +18,12 @@ const admin = require("./src/firebase/index.js");
 const app = express();
 const connectionString = MONGODB_URI;
 const port = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
 // express items
 app.use(cors());
 app.use(bodyParser.json());
@@ -29,42 +34,9 @@ app.use(express.urlencoded({ limit: "50mb", extended: false }));
 
 dbConnection(connectionString);
 
-// Use routes
-// mongoose.set("strictQuery", false);
-// app.get("/api", async (req, res) => {
-//   try {
-//     // Connect to the database
-//     console.log(connectionString, "connection string");
-//     await mongoose.connect(
-//       "mongodb+srv://apnaemitra:Rohit45@cluster0.mmqwssx.mongodb.net/test?retryWrites=true&w=majority",
-//       {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true,
-//       }
-//     );
-//     console.log("Connected to database successfully");
-
-//     // Query the database (assuming 'user' is your mongoose model)
-//     const users = await user.find();
-
-//     // Send response
-//     res.send({
-//       message: "Connected to database successfully++_______..____>>",
-//       users: users,
-//       success: true,
-//       status: 300,
-//     });
-//   } catch (error) {
-//     // Handle errors
-//     res.status(500).send({
-//       message: "Error connecting to database>>>",
-//       error: error.message,
-//       success: false,
-//       status: 600,
-//     });
-//     console.error("Error connecting to database:", error);
-//   }
-// });
+app.get("/", (req, res) => {
+  res.render("index", { pageTitle: "User Interface" });
+});
 
 app.use("/api", route);
 
